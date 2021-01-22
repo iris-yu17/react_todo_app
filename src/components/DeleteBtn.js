75ec2cb7f7ea7024866ae343a5fe728fc8ff90eb
Iris Yu <iris.yu0716@gmail.com>
2021-01-22 12:05:33 +0800

    finish delete function

--- src/components/DeleteBtn.js
+++ src/components/DeleteBtn.js
@@ -1,17 +1,60 @@
 import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import $ from "jquery";
 
-function DeleteBtn() {
-  return (
-    <>
-      <div className="delete">
-        <FontAwesomeIcon icon={faTrash} />
-      </div>
+function DeleteBtn(props) {
+  const { item, todoArray, setTodoArray, delState, setDelState } = props;
 
+  function deleteItem() {
+    // 得到一個已排除被點擊item的陣列
+    let updatedArray = todoArray.filter(function (itemInTodoArray) {
+      return itemInTodoArray.id !== item.id;
+    });
+    //　改變狀態
+    setTimeout(() => {
+       setTodoArray(updatedArray);
+    }, 100);
+   
+  }
 
-    </>
-  );
+  function delBtnMouseDownEffect() {
+    setDelState(1);
+  }
+
+  if (delState === 0) {
+    return (
+      <>
+        <div
+          className="delete"
+          onClick={() => {
+            delBtnMouseDownEffect();
+            deleteItem();
+          }}
+        >
+          <FontAwesomeIcon icon={faTrash} />
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <div
+          className="delete_clicked"
+          onClick={() => {
+            deleteItem();
+          }}
+          onMouseDown={() => {
+            delBtnMouseDownEffect();
+          }}
+        >
+          <div className="fa-trash-delete">
+            <FontAwesomeIcon icon={faTrash} />
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
 export default DeleteBtn;
--- src/components/Item.js
+++ src/components/Item.js
@@ -3,7 +3,8 @@ import DeleteBtn from "./DeleteBtn";
 import CheckBox from "./CheckBox";
 
 function Item(props) {
-  const { item,setItem, todoArray, setTodoArray } = props;
+  const { item, setItem, todoArray, setTodoArray } = props;
+  const [delState, setDelState] = useState(0); // 0:正常樣式, 1:刪除樣式
 
   return (
     <>
@@ -18,7 +19,7 @@ function Item(props) {
           <div className="text">{item.content}</div>
         </div>
 
-        <DeleteBtn />
+        <DeleteBtn  item={item} todoArray={todoArray} setTodoArray={setTodoArray} delState={delState} setDelState={setDelState}/>
       </div>
     </>
   );
